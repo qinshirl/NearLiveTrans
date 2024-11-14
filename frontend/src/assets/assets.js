5ea0f79a922c52aa1c839ed49017ffd162b98427
buruei <buruei@hotmail.com>
2024-11-13 22:59:07 -0500

    assets update

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,11 @@
 import logout from './logout_icon.png'
 import profile_icon from './profile_icon.png'
 import search_icon from './search_icon.png'
+import upload_area from './upload_area.png'
 
 export const assets = {
   logout,
   profile_icon,
-  search_icon
+  search_icon,
+  upload_area,
 }
---
+++ frontend/src/assets/language.js
@@ -0,0 +1,12 @@
+export const Language = {
+  "English": "en",
+  "Spanish": "es",
+  "French": "fr",
+  "German": "de",
+  "Italian": "it",
+  "Japanese": "ja",
+  "Korean": "ko",
+  "Portuguese": "pt",
+  "Russian": "ru",
+  "Chinese": "zh"
+}
