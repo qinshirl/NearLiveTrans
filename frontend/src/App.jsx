4450a9a7a03bba6a333730064116beee5b5601a3
buruei <buruei@hotmail.com>
2024-11-13 22:58:08 -0500

    add toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Dropbox from './pages/Dropbox/Dropbox'
 import { useDispatch, useSelector } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
 import './App.css'
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <>
       <div className='app'>
+        <ToastContainer limit={5} />
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
