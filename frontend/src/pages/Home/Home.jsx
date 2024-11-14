31c94717cf5b15c504b9d09f4b60169a9c8cb745
buruei <buruei@hotmail.com>
2024-11-13 22:59:51 -0500

    Minor testing for reducer

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Home.css'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const text = useSelector(state => state.videoContent.plainText);
+  const videoContent = useSelector(state => state.videoContent.textContent);
   return (
     <div>
       <h1>Home</h1>
+      {text === "" ? <p></p> : <p>{text}</p>}
     </div>
   )
 }
